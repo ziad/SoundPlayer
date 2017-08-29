af4adea7907562a1dfadcd262c0208df3df68bfc
Ziad Hakim <ziad.hakim@holosbase.com>
2017-08-29 13:18:29 +0200

    Initial Commit

---
+++ SoundPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoundPlayer
+//
+//  Created by Ziad Hakim on 29.08.17.
+//  Copyright © 2017 Ziad Hakim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
